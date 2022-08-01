7de2eecdac22c14c1a1cc84283c9025e2386eb4a
ksedixxkp030 <76788207+ksedix@users.noreply.github.com>
2022-08-01 15:13:40 +0200

    Add files via upload
    A label that automatically wraps text when window becomes smaller

---
+++ WrappingLabel.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+
+class WrappingLabel(tk.Label):
+    '''a type of Label that automatically adjusts the wrap to the size'''
+    def __init__(self, master=None, **kwargs):
+        tk.Label.__init__(self, master, **kwargs)
+        self.bind('<Configure>', lambda e: self.config(wraplength=self.winfo_width()))
